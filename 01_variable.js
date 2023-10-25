bf441cdd401e9137ee241ab4c0aa7066a78f80f3
Deependra Kashyap <113233056+Deependrakashya@users.noreply.github.com>
2023-10-25 20:53:32 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+const accountId=23;
+let accountEmail = "this is dkashyap";
+var accountPassword = "codebydk";
+ variable =3987;
+ console.log(variable);
+// accountId =48;
+/*
+ prefer not use var 
+ because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword]);
